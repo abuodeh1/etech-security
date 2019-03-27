7c91a7f3178e7ecddd62c29ed9b2af57ede7f3f5
hassem <hmubaslat@etech-systems.com>
2019-03-27 08:52:44 +0200

    User / Group / Role Implementation

---
+++ src/main/java/etech/admin/domain/User_Role.java
@@ -0,0 +1,28 @@
+package etech.admin.domain;
+
+import javax.persistence.Entity;
+
+
+@Entity(name="user_role")
+public class User_Role {
+
+    private int user_id;
+
+    private int role_id;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+}
